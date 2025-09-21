f00b7be27322a858961d33ffb421066f98e52205
Alex <abuchanan7812@gmail.com>
2025-09-21 16:27:12 +0100

    Added default value level load arguments (Allows for one click level load for fast testing)

--- Include/Core/Menu.hpp
+++ Include/Core/Menu.hpp
@@ -8,7 +8,7 @@ private:
     void sendLoadLevelMessage();
 
 private:
-    char m_levelInputText[256];
-    char m_inclusionOptionsInputText[256];
+    char m_levelInputText[256] = "_pvz/Levels/Mainstreet/Level_COOP_Mainstreet/Level_COOP_Mainstreet";
+    char m_inclusionOptionsInputText[256] = "GameMode=TeamDeathMatch0;TOD=Day;ZDebugMode=AllLevelLoaded";
 
 };
